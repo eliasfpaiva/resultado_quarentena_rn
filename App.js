83b32dca5f523c4f68400e7a4c24bbf02ebb15f5
elias.paiva <elias.paiva@ideagri.com.br>
2020-11-07 22:53:59 -0300

    Estrutura básica da raíz do projeto.

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './componentes/login';
+import Principal from './componentes/principal';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function App() {
+  const [compativel, setCompativel] = useState(false);
+  const [autenticavel, setAutenticavel] = useState(false);
+  const [autenticado, setAutenticado] = useState(false);
+
+  async function verificaCompatibilidade() {
+    let comp = await LocalAuthentication.hasHardwareAsync();
+    if (comp) setCompativel(true);
+    let aut = await LocalAuthentication.isEnrolledAsync();
+    if (aut) setAutenticavel(true);
+  }
+
+  useEffect(() => {
+    verificaCompatibilidade();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      { !compativel || !autenticavel || autenticado ?
+        (<Principal />) :
+        (<Login />)
+      }
       <StatusBar style="auto" />
     </View>
   );
