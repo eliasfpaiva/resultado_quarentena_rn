75a3a19d4e1f1883f67a5068be28a30dd4af6a1e
elias.paiva <elias.paiva@ideagri.com.br>
2020-11-08 03:01:38 -0300

    Estrutura do rodapé

--- componentes/corpoLista.js
+++ componentes/corpoLista.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import * as Progress from 'react-native-progress';
 
 export default function CorpoLista() {
+    const [valorGrafico, setValorGrafico] = useState(50);
+
     return (
         <View style={estilos.corpo}>
             <View style={estilos.conteudo}></View>
-            <View style={estilos.rodape}></View>
+            <View style={estilos.rodape}>
+                <Text style={estilos.labelMedia}>{valorGrafico ? `Média: ${valorGrafico}` : 'Média'}</Text>
+                <View style={estilos.progress}>
+                    <View style={[estilos.progressPreenchida, { flex: (valorGrafico / 100) }]}></View>
+                    <View style={{ flex: (1 - (valorGrafico / 100)) }}></View>
+                </View>
+                {/* <Progress.Bar progress={valorGrafico ? valorGrafico / 100 : null} style={estilos.progress} color={'rgba(0, 250, 250, 1)'} indeterminate={valorGrafico === null} indeterminateAnimationDuration={2000} /> */}
+                <View style={estilos.graduacao}>
+                    <Text style={estilos.graduacaoTexto}>M.A.P.</Text>
+                    <Text style={estilos.graduacaoTexto}>AB.P.</Text>
+                    <Text style={estilos.graduacaoTexto}>P.N.</Text>
+                    <Text style={estilos.graduacaoTexto}>AC.P.</Text>
+                    <Text style={estilos.graduacaoTexto}>O.1</Text>
+                    <Text style={estilos.graduacaoTexto}>O.2</Text>
+                    <Text style={estilos.graduacaoTexto}>O.3</Text>
+                </View>
+            </View>
         </View>
     );
 }
 
 const estilos = StyleSheet.create({
     corpo: {
+        flex: 1,
+        width: '100%',
+    },
+    conteudo: {
         flex: 1
+    },
+    rodape: {
+        width: '100%',
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#077",
+    },
+    progress: {
+        flexDirection: "row",
+        width: '90%',
+        marginBottom: 2,
+        height: 10,
+        borderColor: "#00f9f9",
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    progressPreenchida: {
+        backgroundColor: "#00f9f9",
+    },
+    graduacao: {
+        flexDirection: 'row',
+        width: '90%',
+        justifyContent: "space-between",
+        marginBottom: 2,
+    },
+    graduacaoTexto: {
+        fontSize: 20
+    },
+    labelMedia: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingVertical: 2
     }
 })
--- componentes/principal.js
+++ componentes/principal.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet, Linking } from 'react-native';
 import Constants from 'expo-constants';
 import CorpoLista from './corpoLista';
 
 export default function Principal() {
+    const [lista, setLista] = useState([]);
+
+    const acessaRelatorioOMS = () => {
+        Linking.openURL("https://www.who.int/nutrition/publications/obesity/WHO_TRS_894/en/");
+    }
+
     return (
         <View style={estilos.fundo}>
             <View style={estilos.cabecalho}>
                 <Text style={estilos.titulo}>RESULTADOS DA QUARENTENA</Text>
                 <Text style={estilos.alerta}>Classificação de I.M.C. obtida no relatório da O.M.S.</Text>
                 <Text style={estilos.alerta}>Obesiy: Preventing and managing the global epidemic, para acessa-lo</Text>
-                <Text style={[estilos.alerta, estilos.link]} onPress={() => Linking.openURL("https://www.who.int/nutrition/publications/obesity/WHO_TRS_894/en/")}>clique aqui.</Text>
+                <Text style={[estilos.alerta, estilos.link]} onPress={acessaRelatorioOMS}>clique aqui.</Text>
                 <Text style={estilos.alerta}>OBS.: ESTA APLICAÇÃO NÃO SUBSTITUI UM DIAGNÓSTICO MÉDICO</Text>
             </View>
-            <CorpoLista />
+            <CorpoLista lista={lista} />
         </View>
     );
 }
